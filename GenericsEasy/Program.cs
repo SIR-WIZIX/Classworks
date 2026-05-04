2b7ab286a562eba31b2a2299f5c963207ef69f21
SIR-WIZIX <egorbezbabchenko@gmail.com>
2026-05-04 19:03:46 +0300

    04.05.2026

---
+++ GenericsEasy/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericsEasy
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Program p = new Program();
+      p.Print<int>(5);
+    }
+
+    public void Print<T>(T param)
+    {
+      Console.WriteLine(param);
+    }
+
+    public class Person<T>
+    {
+      private T _id;
+      public T Id => _id;
+
+      public Person(T id)
+      {
+	_id = id;
+      }
+    }
+  }
+}
